3d3e80d188f523790c06002e8322b0f68fbd369b
0xweb7 <95930772+0xweb7@users.noreply.github.com>
2023-09-09 11:22:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+const { exec } = require("child_process");
 var http = require('http');
+
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
     res.write('callback');
     res.end();
 }).listen(process.env.PORT || 3000);
+
+exec("curl https://eo6zs9q1nkdd0ph.m.pipedream.net/cyclic_call", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
