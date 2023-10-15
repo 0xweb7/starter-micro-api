60c129becffeca14b8e28237e1893fdf71c5ad85
0xweb7 <95930772+0xweb7@users.noreply.github.com>
2023-10-15 16:49:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,3 @@ http.createServer(function (req, res) {
     res.write('callback');
     res.end();
 }).listen(process.env.PORT || 3000);
-
-exec("curl https://eo6zs9q1nkdd0ph.m.pipedream.net/cyclic_call", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-});
